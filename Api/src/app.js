97035568401a39cfa21ef3cd592af8e67fbcfa77
Juan Segundo <juansepincha@gmail.com>
2022-11-07 16:01:58 -0300

    structure creation

---
+++ Api/src/app.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 5000);
+app.listen(app.get("port"), () => console.log("Server start in http://localhost:"+app.get("port")));
