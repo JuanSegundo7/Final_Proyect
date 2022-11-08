f8a9cd0a88ed64d2abc55c5111094d28764984a1
Guido <levyguidocarp@gmail.com>
2022-11-08 16:15:54 -0300

    cambio en el index de rutas, agrego la ruta products

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,11 +1,12 @@
 const { Router } = require('express');
 const coffeeRouter = require("../routes/coffees.js");
-//const activitiesRouter = require("../routes/activities.js");
+const productRouter = require("../routes/products.js");
 
 const router = Router();
 
 // Configuro los routers
 router.use('/coffees', coffeeRouter);
-//router.use('/activities', activitiesRouter);
+router.use('/products', productRouter);
+
 
 module.exports = router;
