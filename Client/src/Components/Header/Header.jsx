263119fd02b87770b870fa523d04f6319b7c006e
marcos pla <marcospla413@gmail.com>
2022-11-08 14:13:12 -0300

    card renderizada en home, falta css

--- Client/src/Components/Header/Header.jsx
+++ Client/src/Components/Header/Header.jsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../imagen/img portfolio.png'
+import React from "react";
+import "./Header.css";
+import logo from "../../imagen/img portfolio.png";
 import { SlUserFemale } from "react-icons/sl";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <header id="Header">
-            <nav className='nav-top'>
-                <p><span id='span'>Envíos Gratis </span> a todo el país en compras mayores a 
-                <span id='span'> $5000</span></p>
-            </nav>
-            <nav className='nav'>
-                <img src={logo} alt="logo"/>
-                {/* <Link to='create'> */}
-                    <SlUserFemale className='icono'/> 
-                {/* </Link> */}
-            </nav>
-        </header>
-    );
-}
+  return (
+    <header id="Header">
+      <nav className="nav-top">
+        <p>
+          <span id="span">Envíos Gratis </span> a todo el país en compras
+          mayores a<span id="span"> $5000</span>
+        </p>
+      </nav>
+      <nav className="nav">
+        <img src={logo} alt="logo" />
+        {/* <Link to='create'> */}
+        <SlUserFemale className="icono" />
+        {/* </Link> */}
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- Client/src/redux/Actions/Actions.jsx
+++ Client/src/redux/Actions/Actions.jsx
@@ -1,29 +1,38 @@
 import axios from "axios";
 export const GET_COFFES = "GET_COFFES";
 export const POST_CAFFEE = "POST_CAFFEE";
+export const DELETE_COFFEE = "DELETE_COFFEE";
 
 export const getCoffees = () => {
-    return async function (dispatch){
-     let json = await axios.get(`http://localhost:3001/coffees`)
-     console.log('action data', json.data)
-     return dispatch({
-         type: GET_COFFES,
-         payload: json.data
-     });
+  try {
+    return async function (dispatch) {
+      let json = await axios.get(`http://localhost:3001/coffees`);
+      return dispatch({
+        type: GET_COFFES,
+        payload: json.data,
+      });
     };
- }
- //asd
+  } catch (error) {
+    console.log(error.message, "Error en action");
+  }
+};
+//asd
 
- export const postCoffes = (payload) => {
-    return function (dispatch){
-        axios.post(`http://localhost:3001/coffees`, payload)
-         .then(res => {
-                dispatch({
-                    type: POST_CAFFEE, 
-                    payload: res.data
-                })
-                    console.log('action post', res)
-                })
-        .catch((error) => console.log('error', error))
-    }
- }
+export const postCoffes = (payload) => {
+  return function (dispatch) {
+    axios
+      .post(`http://localhost:3001/coffees`, payload)
+      .then((res) => {
+        dispatch({
+          type: POST_CAFFEE,
+          payload: res.data,
+        });
+        console.log("action post", res);
+      })
+      .catch((error) => console.log("error", error));
+  };
+};
+
+export const deleteCoffee = (id) => {
+  return { type: DELETE_COFFEE, id };
+};
