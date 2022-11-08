1ea16c6bd49a9c65d379c3f3f6dce8f34f344561
Guido <levyguidocarp@gmail.com>
2022-11-08 12:08:25 -0300

    arranco con el modelo de producto y con sus rutas correspondientes

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const coffeeRouter = require("../routes/coffees.js");
+const productRouter = require("../routes/product.js")
 //const activitiesRouter = require("../routes/activities.js");
 
 const router = Router();
@@ -7,5 +8,6 @@ const router = Router();
 // Configuro los routers
 router.use('/coffees', coffeeRouter);
 //router.use('/activities', activitiesRouter);
+router.use("/products" , productRouter);
 
 module.exports = router;
