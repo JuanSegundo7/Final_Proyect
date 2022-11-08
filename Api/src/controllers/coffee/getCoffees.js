bb042c41f8420099d1b6070819b682bff0311dd6
Francisco Javier Caballe <francaballe@gmail.com>
2022-11-08 13:48:19 -0300

    ordenamientos y cambios menores en COFFEE

--- Api/src/controllers/coffee/getCoffees.js
+++ Api/src/controllers/coffee/getCoffees.js
@@ -1,11 +1,23 @@
 const { Coffee } = require("../../db.js")
 
-const getCoffees = async function () {
+const getCoffees = async function (options) {
+
+//en options tengo todo lo que me llega por req.query. Lo tengo que destructurar acá.
+const {name, orderedbyname, orderedbystock} = options;
+const sortOptions = [];
+
+//le hago push a mi array de ordenamientos sólo si tengo algo, caso contrario queda vacío.
+if (orderedbyname && orderedbyname.toUpperCase()==="DES") sortOptions.push(["name", -1])
+if (orderedbyname && orderedbyname.toUpperCase()==="ASC") sortOptions.push(["name"])
+
+if (orderedbystock && orderedbystock.toUpperCase()==="DES") sortOptions.push(["stock", -1])
+if (orderedbystock && orderedbystock.toUpperCase()==="ASC") sortOptions.push(["stock"])
+
+console.log("mi ordenamiento:",sortOptions)
 
   try {
-    const newCoffee =  await Coffee.find({})   
-    return newCoffee;
-
+    const resp = await Coffee.find({ name: new RegExp(name, 'i') }).sort(sortOptions);
+    return resp;
   }catch (unError){
     throw new Error(unError)
   }
--- Api/src/routes/coffees.js
+++ Api/src/routes/coffees.js
@@ -3,8 +3,20 @@ const router = Router();
 const createCoffee = require("../controllers/coffee/createCoffee.js");
 const getCoffees = require("../controllers/coffee/getCoffees.js");
 const deleteCoffee = require("../controllers/coffee/deleteCoffee.js");
-const getCoffeeByName = require("../controllers/coffee/getCoffeeByName.js");
 const updateCoffee = require("../controllers/coffee/updateCoffee.js");
+
+
+router.get("/", async function (req,res){
+    
+    //le voy a pasar a getCoffees todo lo que llega por req.query. Después destruturaré en la 
+    //propia función, no acá.
+    try{
+        const respuesta = await getCoffees(req.query);
+        res.send(respuesta);
+    }catch(unError){
+        res.status(400).send(unError.message);
+    }      
+});
 
 router.put("/:_id", async (req, res) => {
     
@@ -28,28 +40,6 @@ router.post("/", async function (req, res){
     }catch(unError){
         res.status(400).send(unError.message);
     }
-});
-
-router.get("/", async function (req,res){
-    const { name } = req.query;
-    
-    if (name){
-        try{
-            const respuesta = await getCoffeeByName(name);
-            res.send(respuesta);
-        }catch(unError){
-            res.status(400).send(unError.message);
-        }  
-    }
-    else{
-        try {
-            const respuesta = await getCoffees();
-            res.send(respuesta);
-        }catch(unError){
-            res.status(400).send(unError.message);
-        }   
-    }
-    
 });
 
 router.delete("/:_id", async (req, res) => {
