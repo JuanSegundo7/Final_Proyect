87b53e2408f99a435c40a62c620686c186701480
Francisco Javier Caballe <francaballe@gmail.com>
2022-11-09 02:33:29 -0300

    creacion de categorias, validaciones varias y cambios menores. Se sumaron las rutas: createActivity y getActivities

--- Api/src/models/coffee.js
+++ Api/src/models/coffee.js
@@ -1,23 +1,19 @@
 const mongoose = require('mongoose');
 
 const CoffeeSchema = mongoose.Schema(
-  { /* _id: String, */
-    name: {type: String, required: true},
+  { name: {type: String, required: true},
     description: {type: String, required: true},
     origin: {type: String, required: true},
     type: {type: String, 
-    enum : ["En Grano", "Molienda Gruesa", "Molienda Media", "Molienda Fina"]},
+      enum : ["En Grano", "Molienda Gruesa", "Molienda Media", "Molienda Fina"]},
     stock: Number,
-    category: {type: Number, required: true},
-    /* activities: [
-      {
+    category: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Activity" //Esta es la forma de hacer las cosas con Mongoose y BBDD no relacionales.
-      }
-    ] */
+        required: true,
+        ref: "Category" //Esta es la forma de hacer las cosas con Mongoose y BBDD no relacionales.
+    }
   },
-  {versionKey: false},
-  { timestamps: true }
+  {versionKey: false}
   );
 
 const Coffee = mongoose.model("Coffee",CoffeeSchema) 
