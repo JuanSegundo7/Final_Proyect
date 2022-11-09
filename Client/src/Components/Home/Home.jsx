8f470d80bdfc6da2fcfabd733b49113344b0ce0b
marcos pla <marcospla413@gmail.com>
2022-11-09 15:36:54 -0300

    components

--- Client/src/Components/Home/Home.jsx
+++ Client/src/Components/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <>
       {/* <ContenidoCoffes /> */}
-      <ContenidoProducts />
+      {/* <ContenidoProducts /> */}
       <About />
     </>
   );
