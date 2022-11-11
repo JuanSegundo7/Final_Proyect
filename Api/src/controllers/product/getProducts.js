c7bf903093f188406ee8c20228cba6edbb4b62a6
Francisco Javier Caballe <francaballe@gmail.com>
2022-11-11 19:34:30 -0300

    agregado el filtro de productos por categoria

--- Api/src/controllers/product/getProducts.js
+++ Api/src/controllers/product/getProducts.js
@@ -1,9 +1,10 @@
-const { Product } = require("../../db.js")
+const { Product, Category } = require("../../db.js")
 
 const getProducts = async function (options) {
 
 //en options tengo todo lo que me llega por req.query. Lo tengo que destructurar acá.
 const {name, orderedbyname, orderedbystock, orderedbyprice} = options;
+let { category } = options;
 const sortOptions = [];
 
 //le hago push a mi array de ordenamientos sólo si tengo algo, caso contrario queda vacío.
@@ -16,8 +17,21 @@ if (orderedbystock && orderedbystock.toUpperCase()==="ASC") sortOptions.push(["s
 if (orderedbyprice && orderedbyprice.toUpperCase()==="DES") sortOptions.push(["price", -1])
 if (orderedbyprice && orderedbyprice.toUpperCase()==="ASC") sortOptions.push(["price"])
 
+//busco dentro de categories aquellas que coincidan en nombre con lo que le paso en category
+try{
+  var categoryComplete = await Category.findOne({name: category})
+}catch(unError){
+  throw new Error(unError)
+}
+if (categoryComplete){
+  category =  categoryComplete._id
+}else{
+  category = null;
+}
+
   try {
-    const resp = await Product.find({ name: new RegExp(name, 'i') }).sort(sortOptions)
+    const resp = await Product.find({ name: new RegExp(name, 'i') },{category})
+    .sort(sortOptions)
     .populate("image").populate("category").populate("brand");
     return resp;
   }catch (unError){
