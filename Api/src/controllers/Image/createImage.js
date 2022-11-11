c23872e1ba1756a62fa1562458424113caa00964
EmanuelCorreaAR <correa.emanuel@outlook.com>
2022-11-11 11:43:24 -0300

    Validacion regex URL create Image-chau .db.txt

--- Api/src/controllers/Image/createImage.js
+++ Api/src/controllers/Image/createImage.js
@@ -1,9 +1,23 @@
-const {Image} = require('../../db.js');
+const { Image } = require('../../db.js');
 
-const createImage = async function(data){
-    const {url,name} = data;
-    const respuesta = await Image.create({url, name});
-    return respuesta;
+
+const createImage = async function (data) {
+    const { url, name } = data;
+    let regex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+    //Data validation
+    if ((typeof (name) !== "string") || (!name.length)) {
+        throw new Error("Error: Image Name cannot be empty and must be of text type.")
+    }
+    if (!regex.test(url)) {
+        throw new Error("Error: Image not URL.")
+    }
+    //si no hay error subo la imagen
+    try {
+        const newImage = await Image.create({ url, name });
+        return newImage;
+    } catch (unError) {
+        throw new Error(unError)
+    }
 }
 
 module.exports = createImage;
