ec773a290740a413b61226ba4e6c6bf21ef0aff6
Juan Segundo <juansepincha@gmail.com>
2022-11-13 22:17:05 -0300

    adding design to a lot of things

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -4,8 +4,25 @@ import Error from "./imgs/error.webp";
 import "./Card.css";
 
 export function Card(props) {
-  return (
-    <div id="card">
+  console.log(props); 
+  if(props.brand === "Brands"){
+    return (
+      <div id="card">
+          <img
+            src={!props.img && props.img2 ? props.img2 : props.img}
+            alt="img not found"
+            className="imgCard_brand"
+            onError={({ currentTarget }) => {
+              currentTarget.onerror = null;
+              currentTarget.src = Error;
+            }}
+            />
+        <p className="brand_name">{props.name ? props.name.toUpperCase() : "There is not name provided"}</p>
+      </div>
+    )
+  } else {
+    return (
+      <div id="card">
       <Link to={`/detail/${props._id}`} id="a_card">
         <img
           src={!props.img && props.img2 ? props.img2 : props.img}
@@ -15,12 +32,13 @@ export function Card(props) {
             currentTarget.onerror = null;
             currentTarget.src = Error;
           }}
-        />
+          />
       </Link>
       <p id="name">{props.name ? props.name : "There is not name provided"}</p>
       <p id="origin">{props.origin}</p>
       <p>US$ {props.price}</p>
       <input type="submit" className="button" value="Add to cart" />
     </div>
-  );
+    )
+  }
 }
