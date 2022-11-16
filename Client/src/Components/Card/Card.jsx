bd013ed31c4c7dfb58aa4d60b3d8003e7c4e10b8
Juan Segundo <juansepincha@gmail.com>
2022-11-15 23:12:09 -0300

    adding favorites semi functional component

--- Client/src/Components/Card/Card.jsx
+++ Client/src/Components/Card/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Error from "./imgs/error.webp";
+import Favorite from "../Favorite/Favorite"
 import "./Card.css";
 
 export function Card(props) {
@@ -24,6 +25,7 @@ export function Card(props) {
   } else {
     return (
       <div id="card">
+        <Favorite id={props._id} />
         <Link to={`/detail/${props._id}`} id="a_card">
           <img
             src={!props.img && props.img2 ? props.img2 : props.img}
@@ -41,7 +43,7 @@ export function Card(props) {
         {props.origin ? <p id="origin">{props.origin}</p> : null}
         <p id="price">US$ {props.price}</p>
         <button type="submit" className="button">
-          Add to cart{" "}
+          Add to cart
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
             <path d="M24 0C10.7 0 0 10.7 0 24S10.7 48 24 48H76.1l60.3 316.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24-10.7 24-24s-10.7-24-24-24H179.9l-9.1-48h317c14.3 0 26.9-9.5 30.8-23.3l54-192C578.3 52.3 563 32 541.8 32H122l-2.4-12.5C117.4 8.2 107.5 0 96 0H24zM176 512c26.5 0 48-21.5 48-48s-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48zm336-48c0-26.5-21.5-48-48-48s-48 21.5-48 48s21.5 48 48 48s48-21.5 48-48z" />
           </svg>
