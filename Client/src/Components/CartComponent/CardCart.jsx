0d329c6ecbb7f052997e73b4a15425ab6a164a84
marcos pla <marcospla413@gmail.com>
2022-11-27 19:26:07 -0300

    tengo que hacer pull

--- Client/src/Components/CartComponent/CardCart.jsx
+++ Client/src/Components/CartComponent/CardCart.jsx
@@ -1,4 +1,3 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
--- Client/src/Components/Header/Header.js
+++ Client/src/Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./img/coffee.png";
 import Categories from "../Categories/Categories";
 import { Link as Navigator } from "react-router-dom";
@@ -6,16 +6,22 @@ import "./Header.css";
 import SearchBar from "../SearchBar/SearchBar";
 import { useDispatch, useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
-import { postUser, getOneUser, updateUser, setAllFavorites, findAllCart } from "../../redux/Actions/Actions";
+import {
+  postUser,
+  getOneUser,
+  updateUser,
+  setAllFavorites,
+  findAllCart,
+} from "../../redux/Actions/Actions";
 import Error from "./img/user_not_found_white.png";
-
 
 const Header = () => {
   const dispatch = useDispatch();
+  const datosEnMiBD = useSelector((state) => state.User);
   const Favorites = useSelector((state) => state.Favorites);
   const allCart = useSelector((state) => state.cart);
   const { user, isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
 
   //Local Storages
   const myLocalStgFavorites = localStorage.getItem("Favorites-pf");
@@ -32,7 +38,6 @@ const Header = () => {
     }
   }, [user]);
 
-  const datosEnMiBD = useSelector((state) => state.User);
   useEffect(() => {
     if (datosEnMiBD.hasOwnProperty("_id")) {
       /*********************Merging Favorites from my DB and LocalStorage******************/
@@ -97,10 +102,10 @@ const Header = () => {
       //console.log("Datos de mi BD del error:",datosEnMiBD);
     }
   }, [datosEnMiBD]);
-  
-const about = window.location.pathname === "/about";
-// `${about?"display:none" : "display:block"}`
-// console.log(about);
+
+  const about = window.location.pathname === "/about";
+  // `${about?"display:none" : "display:block"}`
+  // console.log(about);
 
   return (
     <header className="display:none">
@@ -130,7 +135,15 @@ const about = window.location.pathname === "/about";
           ) : (
             <Navigator to="/profile">
               <div className="svg-container">
-                <img id={!error ? "user-img" : "error-img"} src={user.picture} onError={({ currentTarget }) => {currentTarget.onerror = null; currentTarget.src = Error; setError(true)}} />
+                <img
+                  id={!error ? "user-img" : "error-img"}
+                  src={user.picture}
+                  onError={({ currentTarget }) => {
+                    currentTarget.onerror = null;
+                    currentTarget.src = Error;
+                    setError(true);
+                  }}
+                />
               </div>
             </Navigator>
           )}
@@ -198,9 +211,7 @@ const about = window.location.pathname === "/about";
       </nav>
       <Categories />
     </header>
-    
   );
-
 };
 
 export default Header;
