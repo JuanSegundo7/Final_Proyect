71286db86df727d447c2b0ed33dd4887327791ab
marcos pla <marcospla413@gmail.com>
2022-11-29 18:32:24 -0300

    quit the lorem and put text

--- Client/src/Components/About/About.jsx
+++ Client/src/Components/About/About.jsx
@@ -14,15 +14,11 @@ export default function About() {
           <img className="logo" src={logo} alt="logo" />
         </a>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          We are Henry graduates who, in the final project instance, decided to
+          start an e-commerce because most of us like coffee. It was a very nice
+          stage where we learned how to work as a team and overcome different
+          conflicts. For this project we decided to use the following
+          technologies: React, Redux, Mongoose/MongoDb, Javascript and Css
         </p>
       </div>
       <section className="cards">
