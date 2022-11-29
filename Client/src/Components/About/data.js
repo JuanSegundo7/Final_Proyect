42e550c7947331fd81fc8d29920494c4fd30bd47
marcos pla <marcospla413@gmail.com>
2022-11-29 14:34:56 -0300

    photo change

--- Client/src/Components/About/data.js
+++ Client/src/Components/About/data.js
@@ -1,38 +1,38 @@
 export const data = [
-    {
-        name: "Marcos Pla Señorans",
-        img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669379307/foto_para_cv_-_Marcos_Pla_xfesvw.jpg",
-        linkedin:"https://www.linkedin.com/in/marcos-pla",
-        github:"https://github.com/marcospla02"
-    },
-    {
-        name: "Guido Levy",
-        img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669587490/wmi9mnljgccauhy5aejc.jpg",
-        linkedin:"https://www.linkedin.com/in/guido-levy-147640246",
-        github:"https://github.com/Guidolevy23"
-    },
-    {
-        name: "Francisco Javier Caballe",
-        img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669301390/tibxu0mox4vrlnefs1sy.jpg",
-        linkedin:"https://www.linkedin.com/in/francisco-caballe/",
-        github:"https://github.com/francaballe"
-    },
-    {
-        name: "Juan Segundo Martinez",
-        img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669735611/Juanse_-_Juan_Segundo_Mart%C3%ADnez._vawx3f.jpg",
-        linkedin:"https://www.linkedin.com/in/juansegundomartinez/",
-        github:"https://github.com/JuanSegundo7"
-    },
-    {
-        name: "Francisco Porta",
-        img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669735244/perfil_-_Francisco_Porta_z2049j.jpg",
-        linkedin:"https://www.linkedin.com/in/franciscoporta/",
-        github:"https://github.com/franciscoporta"
-    },
-    {
-        name: "Emanuel Correa",
-        img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669318784/1566314615583_suzaep.jpg",
-        linkedin:"https://www.linkedin.com/in/emanuel-s-correa-5b4b40139",
-        github:"https://github.com/EmanuelCorreaAR"
-    }
-]
+  {
+    name: "Marcos Pla Señorans",
+    img: "https://res.cloudinary.com/dczcouh4i/image/upload/v1669742583/foto_linkedIn_feyxpk.jpg",
+    linkedin: "https://www.linkedin.com/in/marcos-pla",
+    github: "https://github.com/marcospla02",
+  },
+  {
+    name: "Guido Levy",
+    img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669587490/wmi9mnljgccauhy5aejc.jpg",
+    linkedin: "https://www.linkedin.com/in/guido-levy-147640246",
+    github: "https://github.com/Guidolevy23",
+  },
+  {
+    name: "Francisco Javier Caballe",
+    img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669301390/tibxu0mox4vrlnefs1sy.jpg",
+    linkedin: "https://www.linkedin.com/in/francisco-caballe/",
+    github: "https://github.com/francaballe",
+  },
+  {
+    name: "Juan Segundo Martinez",
+    img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669735611/Juanse_-_Juan_Segundo_Mart%C3%ADnez._vawx3f.jpg",
+    linkedin: "https://www.linkedin.com/in/juansegundomartinez/",
+    github: "https://github.com/JuanSegundo7",
+  },
+  {
+    name: "Francisco Porta",
+    img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669735244/perfil_-_Francisco_Porta_z2049j.jpg",
+    linkedin: "https://www.linkedin.com/in/franciscoporta/",
+    github: "https://github.com/franciscoporta",
+  },
+  {
+    name: "Emanuel Correa",
+    img: "https://res.cloudinary.com/drscelx6f/image/upload/v1669318784/1566314615583_suzaep.jpg",
+    linkedin: "https://www.linkedin.com/in/emanuel-s-correa-5b4b40139",
+    github: "https://github.com/EmanuelCorreaAR",
+  },
+];
