a72f141e3f2e5633006786e87a9cd338c0f5a902
Juan Segundo <juansepincha@gmail.com>
2022-11-29 18:36:51 -0300

    fixing dashboard

--- Client/src/Components/ProductForm/ModalUsers.jsx
+++ Client/src/Components/ProductForm/ModalUsers.jsx
@@ -1,14 +1,11 @@
-import React, { useState } from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {updateProduct} from "../../redux/Actions/Actions"
+import React from 'react';
+import {useDispatch} from "react-redux";
+import {updateUser} from "../../redux/Actions/Actions"
 
-const ModalComponent = ({openModal, closeModal, info, close}) => {
+const ModalComponent = ({info, close}) => {
   const dispatch = useDispatch();
-  const category = useSelector((state) => state.Categories);
-  const brand = useSelector((state) => state.Brands)
-  const [categoryId, setCategoryId] = useState(info.category2);
-  const [brandId, setBrandId] = useState(info.brand2)
 
+  console.log(info)
 
   const handleChange=(e)=>{
     console.log(e, "E")
@@ -21,37 +18,17 @@ const ModalComponent = ({openModal, closeModal, info, close}) => {
   }
 
   const [selectedProduct, setSelectedProduct] = React.useState({
-    name: info.name,
-    brand: categoryId.brand,
-    category: categoryId.category,
-    description: info.description,
-    price: info.price,
-    stock: info.stock
+    name: info.name, 
+    lastname: info.lastname
   })
 
-  //console.log('selectedProduct', selectedProduct.category.name)
-
-  const handleCategory = (e ) => {
-    //console.log('e', e.target.value)
-    setCategoryId(e.target.value);
-  }
-
-  const handleBrand = (e ) => {
-    //console.log('e', e.target.value)
-    setBrandId(e.target.value);
-  }
 
   const handleSubmit = (e) => {
     let data = {
       name: selectedProduct.name,
-      brand: brandId,
-      category: categoryId,
-      description: selectedProduct.description,
-      price: parseInt(selectedProduct.price),
-      stock: parseInt(info.stock)
+      lastname: selectedProduct.lastname
     }
-    console.log('data',data)
-    dispatch(updateProduct(info.id, data))
+    dispatch(updateUser(info.id, data))
     close(false)
     e.preventDefault();
   }
@@ -63,40 +40,19 @@ const ModalComponent = ({openModal, closeModal, info, close}) => {
         <article id="wrapper-x" onClick={() => close(false)}> 
           X
         </article>
-        <form>
+        <form id="form-user">
           <fieldset>
             <label>Name</label>
             <input  name="name" onChange={e => handleChange(e)} value={selectedProduct.name}></input>
           </fieldset>
           <fieldset>
-            {/* <label>Category</label>
-            <input label="category" name="category" onChange={e => handleChange(e)} value={selectedProduct.category}></input> */}
-            <select onChange={(e) => handleCategory(e)}>
-              {
-                category?.map((c) => <option key={c._id} value={c._id}>{c.name}</option>)
-              }
-            </select>
+            <label>Last name</label>
+            <input name="lastname" onChange={e => handleChange(e)} value={selectedProduct.lastname}></input>
           </fieldset>
-          {/* <fieldset>
-            <label>Brand</label>
-            <input label="brand" name="brand" onChange={e => handleChange(e)} value={selectedProduct.brand}></input>
-          </fieldset> */}
-          <select onChange={(e) => handleBrand(e)}>
-            {
-              brand?.map((b) => <option key={b._id} value={b._id}>{b.name}</option>)
-            }
-          </select>
-          <fieldset>
-            <label>Price</label>
-            <input label="price" name="price" onChange={e => handleChange(e)} value={selectedProduct.price}></input>
+          <fieldset id="flex-buttons-modal">
+            <button className="button-dashboard" onClick={handleSubmit}>Edit</button>
+            <button className="button-dashboard" onClick={() => close(false)}>Cancel</button>
           </fieldset>
-          <fieldset>
-            <label>Stock</label>
-            <input label="stock" name="stock" onChange={e => handleChange(e)} value={selectedProduct.stock}></input>
-          </fieldset>
-          <textarea name="description" onChange={e => handleChange(e)} value={selectedProduct.description}></textarea>
-          <button onClick={handleSubmit}>Edit</button>
-          <button onClick={() => close(false)}>Cancel</button>
         </form>
     </article>
     </article>
