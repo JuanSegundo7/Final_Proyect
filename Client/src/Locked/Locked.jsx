0e75b0a276004c9e1333a2173d3c61fa01e0242e
marcos pla <marcospla413@gmail.com>
2022-11-30 20:50:18 -0300

    contact us in locked

--- Client/src/Locked/Locked.jsx
+++ Client/src/Locked/Locked.jsx
@@ -1,23 +1,12 @@
-import { useAuth0 } from "@auth0/auth0-react";
+// import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-//import Image1 from "./Locked.jpg";
 import "./Locked.css";
+import Switch from "./switch/Switch";
 
 const Locked = () => {
-  const { logout } = useAuth0();
-
-  const logoutHandle = () => {
-    logout({ returnTo: window.location.origin });
-  };
-
   return (
     <div>
-      <div id="Locked">
-        <button id="button-contact">Contact support</button>
-        <button id="button-logout" onClick={logoutHandle}>
-          Logout
-        </button>
-      </div>
+      <Switch />
     </div>
   );
 };
